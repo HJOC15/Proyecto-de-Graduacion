7c492d008846755ee2ac7287f7db6dd5415dd041
HJOC15 <humbertorellana15@gmail.com>
2023-06-09 02:04:15 -0600

    Orden de pago implementada

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, ViewChild } from '@angular/core';
 import { LatLng, LatLngExpression, LatLngTuple, LeafletMouseEvent, Map, Marker, icon, latLng, map, marker, tileLayer } from 'leaflet';
 import { LocationService } from 'src/app/services/location.service';
 import { Order } from 'src/app/shared/models/Order';
@@ -8,10 +8,12 @@ import { Order } from 'src/app/shared/models/Order';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit{
+export class MapComponent implements OnChanges{
 
 @Input()
 order!:Order
+@Input()
+readonly = false;
 private readonly MARKER_ZOOM_LEVEL = 16;
 private readonly MARKER_ICON = icon({
   iconUrl:
@@ -30,8 +32,29 @@ constructor(private locationService: LocationService){
 
 }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+
+    if(!this.order) return;
     this.initializeMap();
+
+    if(this.readonly &&  this.addressLatLng){
+      this.showLocationReadOnlyMode();
+    }
+  }
+  showLocationReadOnlyMode() {
+    const m = this.map;
+    this.setMarker(this.addressLatLng)
+    m.setView(this.addressLatLng, this.MARKER_ZOOM_LEVEL)
+
+    m.dragging.disable();
+    m.touchZoom.disable();
+    m.doubleClickZoom.disable();
+    m.scrollWheelZoom.disable();
+    m.boxZoom.disable();
+    m.keyboard.disable();
+    m.off('click');
+    m.tap?.disable;
+    this.currentMarker.dragging?.disable();
   }
 
   initializeMap(){
@@ -75,10 +98,17 @@ constructor(private locationService: LocationService){
   }
 
   set addressLatLng(latLng: LatLng){
+
+    if(!latLng.lat.toFixed) return;
+
     latLng.lat = parseFloat(latLng.lat.toFixed(8))
     latLng.lng = parseFloat(latLng.lng.toFixed(8))
     this.order.addressLatLng = latLng;
     console.log(this.order.addressLatLng);
+  }
+
+  get addressLatLng(){
+    return this.order.addressLatLng!;
   }
 
 }
